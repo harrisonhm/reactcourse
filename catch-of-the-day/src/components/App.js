f5e1bcf2dbe5b06fe035659e386fb1f3f6da08a6
Harrison Holt-McHale <hholtmchale@yozu.co.uk>
2018-09-21 15:22:04 +0100

    Finished Chapter 21

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -46,6 +46,12 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  deleteFish = (key) => {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = null
+    this.setState({ fishes })
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -58,6 +64,13 @@ class App extends React.Component {
     // 3. Call setState to update our state object
     this.setState({ order });
   };
+
+  removeFromOrder = key => {
+    const order = { ...this.state.order };
+    delete order[key]
+    this.setState({ order })
+  }
+
 
   render() {
     return (
@@ -75,10 +88,11 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
